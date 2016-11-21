aa2152f743fed260ff22614e42d76e08af346375
Liuxudong <17801036039@163.com>
2016-11-21 23:20:28 +0800

    nothing

--- src/thinkstats2.py
+++ src/thinkstats2.py
@@ -1,6 +1,18 @@
 # coding: utf-8
 
 """
-this is test
+This file contain code for use with "Think Stats" and "Think Bayes", both by Allen B. Downey,
+available form greenteapress.com
 
+Copyright 2014 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
+
+
+def read_stata_dct(dct_file, **op):
+    """
+    Read a Stata dictionary file.
+    :param dct_file: doc
+    :param op:
+    :return:
+    """
