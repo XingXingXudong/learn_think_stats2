4526dd988e0fb121a1a9ddb56708b3c2a90579d5
Liuxudong <17801036039@163.com>
2016-11-24 23:25:17 +0800

    人生何处不缝坑

--- src/thinkstats2.py
+++ src/thinkstats2.py
@@ -8,6 +8,7 @@ Copyright 2014 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
+import re
 
 def read_stata_dct(dct_file, **op):
     """
@@ -16,3 +17,25 @@ def read_stata_dct(dct_file, **op):
     :param op:
     :return:
     """
+    type_map = dict(byte=int, int=int, long=int, float=float, double=float)
+
+    var_info = []
+
+    for line in open(dct_file, **options):
+        match = re.search(r'_column\(([^)]*)\)', line)
+        if match:
+            start = int(match.group(1))
+            t = line.split()
+            vtype, name, fstring = t[1:4]
+            name = name.lower()
+            if vtype.startwith('str'):
+                vtype = str
+            else:
+                vtype = type.map[vtype]
+            long_desc = ' '.join(t[4:]).strip('"')
+            var_info.append((start, vtype, name, fstring, long_desc))
+
+    columns = ['start', 'type', 'name', 'fstring', 'desc']
+    variables = pandas.DataFrame(var_info, )
+
+
