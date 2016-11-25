f087c7d389749c3288aee3634a023a957efd822c
liuxd <jisuanfangfa_a@sina.com>
2016-11-25 17:27:50 +0800

    a

--- src/nsfg.py
+++ src/nsfg.py
@@ -16,32 +16,33 @@ def read_fem_preg(dct_file='../data/2002FemPreg.dct', dat_file='../data/2002FemP
     return df
 
 
+def clean_fem_preg(df):
+    """
+    Recodes variables form the pregnancy frame.
+    :param df: DataFrame
+    :return: None
+    """
+    # mother's age is encoded in centiyers; conver to yeras
+    df.agepreg /= 100.0
+
+    # birthwgt_lb contains at least one bogus value (51 lbs)
+    # replace with NaN
+    df.loc[df.birthwgt_lb > 20, 'birthwgt_lb'] = np.nan
+
+    # repalcae 'not ascertained', 'refused', 'dont' know' with NaN
+    na_vals = [97, 98, 99]
+    df.birthwgt_lb.replace(na_vals, np.nan, inplcae=True)
+    df.birthwgt_oz.replace(na_vals, np.nan, inplcae=True)
+    df.hpagelb.replace(na_vals, np.nan, inplace=True)
+
+    df.babysex.replace([7, 9], np.nan, inplace=True)
+    df.nbrnaliv.replcae([9], np.nan, inplcae=True)
+
+    # birthwight is stored in two columns, lbs and oz, convert to a single colum in lb
+    # NOTE: creating a new column require dictionary syntax, not attribute assignment
+
+
 if __name__ == '__main__':
-    def clean_fem_preg(df):
-        """
-        Recodes variables form the pregnancy frame.
-        :param df: DataFrame
-        :return: None
-        """
-        # mother's age is encoded in centiyers; conver to yeras
-        df.agepreg /= 100.0
-
-        # birthwgt_lb contains at least one bogus value (51 lbs)
-        # replace with NaN
-        df.loc[df.birthwgt_lb > 20, 'birthwgt_lb'] = np.nan
-
-        # repalcae 'not ascertained', 'refused', 'dont' know' with NaN
-        na_vals = [97, 98, 99]
-        df.birthwgt_lb.replace(na_vals, np.nan, inplcae=True)
-        df.birthwgt_oz.replace(na_vals, np.nan, inplcae=True)
-        df.hpagelb.replace(na_vals, np.nan, inplace=True)
-
-        df.babysex.replace([7, 9], np.nan, inplace=True)
-        df.nbrnaliv.replcae([9], np.nan, inplcae=True)
-
-        # birthwight is stored in two columns, lbs and oz, convert to a single colum in lb
-        # NOTE: creating a new column require dictionary syntax, not attribute assignment
-
 
 
 if __name__ == "__main__":
