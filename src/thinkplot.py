4c19dd8ba7398cd3a4deaa373b168cedd009737f
Liuxudong <17801036039@163.com>
2016-11-28 23:50:26 +0800

    -m回头无岸

--- src/thinkplot.py
+++ src/thinkplot.py
@@ -53,8 +53,72 @@ def _UnderrideColor(options):
 
 
 class _Brewer(object):
-    pass
+    """
+    Encapsulates a nice sequence of colors
 
+    Shades of blue that look lood in color and canbe distinguishe in grayscale(up to point).
+    Borrowed fro m http://colorbrewer2.org
+    """
+    color_iter = None
+    colors = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecael', '#4292c6',
+              '#2171b5', '#08519c', '#08306b'][::-1]
+    # lists that indicate which colors to use depending on how many are used
+    which_colors = [[],
+                    [1],
+                    [1, 3],
+                    [0, 2, 4],
+                    [0, 2, 4, 6],
+                    [0, 2, 3, 5, 6],
+                    [0, 1, 2, 3, 4, 5, 6],
+                    [0, 1, 2, 3, 4, 5, 6, 7],
+                    [0, 1, 2, 3, 4, 5, 6, 7, 8]]
+
+    current_figure = None
+
+    @classmethod
+    def Colors(cls):
+        """
+        Returns the lsit of colors.
+        :return: colors
+        """
+        return cls.colors
+
+    @classmethod
+    def ColorGenerator(cls, num):
+        """
+        Returns an iterator of color strings.
+        :param num: how many colors will be used
+        :return: colors
+        """
+        for i in cls.which_colors[num]:
+            yield cls.colors[i]
+        raise StopIteration('Ran out of colors in _Brewer')
+
+    @classmethod
+    def InitIter(cls, num):
+        """
+        Initializes the color iterator with the given number of colors.
+        :param num: how many colors will be used
+        """
+        cls.color_iter = cls.ColorGenerator(num)
+
+    @classmethod
+    def ClearIter(cls):
+        """Sets the color iterator to None."""
+        cls.color_iter = None
+
+    @classmethod
+    def GetIter(cls, num):
+        """Gets the color iterator."""
+        fig = pyplot.gcf()  # pyplot.gcf()get a reference to the current figure.
+        if fig != cls.current_figure:
+            cls.InitIter(num)
+            cls.current_figure = fig
+
+        if cls.color_iter is None:
+            cls.InitIter(num)
+
+        return cls.color_iter
 
 
 def Bar(xs, ys, **options):
@@ -65,7 +129,8 @@ def Bar(xs, ys, **options):
     :param options: keyword args passed to pyplot.bar
     """
     options = _UnderrideColor(options)
-
+    options = _Underride(options, linewidth=0, alpha=0.6)
+    pyplot.bar(xs, ys, **options)
 
 
 def Hist(hist, **options):
@@ -97,5 +162,45 @@ def Hist(hist, **options):
         options['width'] *= -1
 
     Bar(xs, ys, **options)
+
+
+def Config(**options):
+    """
+    Configures the plot.
+    Pull options out of the options dictionary and passes them to the corresponding pyplot functions.
+    :param options: keyword args to pyplot functions
+    """
+    names = ['title', 'xlabel', 'ylabel', 'xscale', 'yscale',
+             'xticks', 'yticks', 'axis', 'xlim', 'ylim']
+
+    for name in names:
+        if name in options:
+            getattr(pyplot, name)(options[name])
+
+    global LEGEND
+    LEGEND = options.get('legend', LEGEND)
+
+    if LEGEND:
+        global LOC
+        LOC = options.get('loc', LOC)
+        pyplot.legend(loc=LOC)
+
+    val = options.get('xticklabels', None)
+
+
+def Show(**options):
+    """
+    Shows the plot.
+    For options, see Config.
+    :param options: keyword args used to inovke various pyplot functions
+    """
+    clf = options.pop('clf', True)
+    Config(**options)
+    pylot.show()
+    if clf:
+        Clf()
+
+
+
 
 
---
+++ test/test2.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+"""
+Chapter 2
+"""
+
+import src.thinkplot as thinkplot
+import src.thinkstats2 as thinkstats2
+
+hist = thinkstats2.Hist([1, 2, 2, 3, 5])
+thinkplot.Hist(hist)
